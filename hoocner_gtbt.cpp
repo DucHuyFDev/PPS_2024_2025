75bea892f26bb6602403ced82c4df295ab488e9c
DucHuyFDev <leduchuy13803@gmail.com>
2024-11-20 01:18:17 +0700

    First

---
+++ hoocner_gtbt.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Code tính giá trị biểu thức theo phương pháp Horner
+
+float tinhGiaTri_hoocner(vector<float> v, float x){
+	float result = v[0];
+	for (int i = 1; i < v.size(); i++){
+		result = result * x + v[i];
+	}
+	return result;
+}
+
+int main(){
+	int n;
+	cout << "Nhap bac cua da thuc: ";	
+	cin >> n;
+	vector<float>v;
+	for (int i = n; i >= 0; i--){
+		float tmp;
+		if (i){
+			cout << "Nhap he so bac " << i << " cua da thuc: ";
+			cin >> tmp;
+			v.push_back(tmp);
+		}
+		else{
+			cout << "Nhap he so tu do: ";
+			cin >> tmp;
+			v.push_back(tmp);
+		}
+	}
+	float x;
+	cout << "Nhap gia tri x = ";
+	cin >> x;
+	cout << "Gia tri cua da thuc tai x = " << x << " la: " << tinhGiaTri_hoocner(v,x);
+	return 0;
+}
